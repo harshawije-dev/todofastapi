7a263c079111cca15764c4211d574245205941a1
Harsha_Wije <harsha.bro1997@gmail.com>
2024-03-10 00:49:07 +0530

    update endpoints

---
+++ main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+from fastapi.params import Body
+from typing import Union
+
+app = FastAPI()
+
+# Defining index route
+
+
+@app.get('/')
+def _root():
+    return {
+        "message": "Success",
+        "response": "Hello World"
+    }
+
+
+@app.get('/todos/{product_id}')
+async def _get_products(product_id: int, q: Union[str, None] = None):
+    return {
+        "message": "Success",
+        "id": product_id,
+        "q": q
+    }
+
+
+@app.post('/todo/create')
+async def _create_todo(payload: dict = Body(...)):
+    return {
+        "message": "Success",
+        "data": payload
+    }
